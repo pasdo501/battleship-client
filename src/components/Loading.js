59c6125b386ff68807b4829a2ca7afc2b4b60d8b
pasdo501 <dominikpaschke@hotmail.com>
2019-08-04 16:51:22 +1200

    Allow passing extra attrs to Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 import { wrapper } from "./styles/Loading.module.scss";
 
-export default function Loading({ text = "Loading", speed = 300 }) {
+export default function Loading({ text = "Loading", speed = 300, ...attrs }) {
   const [content, setContent] = React.useState(text);
   React.useEffect(() => {
     const intervalId = window.setInterval(() => {
@@ -17,7 +17,7 @@ export default function Loading({ text = "Loading", speed = 300 }) {
   }, [text, speed]);
 
   return (
-    <div className={wrapper}>
+    <div className={wrapper} {...attrs}>
       <h2>{content}</h2>
     </div>
   );
