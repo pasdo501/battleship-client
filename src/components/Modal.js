705db5c636454e38b1c009f61d2545057486d4bf
pasdo501 <dominikpaschke@hotmail.com>
2019-08-05 09:05:45 +1200

    Modal updates
    - Makes the modal close button optional
    - Extracted transition speed from the scss file & made it changeable
    with props (also consistent now)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,13 @@ import PropTypes from "prop-types";
 
 import styles from "./styles/Modal.module.scss";
 
-export default function Modal({ handleHide, children }) {
+export default function Modal({
+  children,
+  handleHide = null,
+  withCloseButton = true,
+  speed = 500,
+}) {
+  console.log(speed)
   const [fadeIn, setFadeIn] = React.useState(false);
   const [hide, setHide] = React.useState(false);
 
@@ -16,19 +22,22 @@ export default function Modal({ handleHide, children }) {
 
     const id = window.setTimeout(() => {
       handleHide();
-    }, 500);
+    }, speed);
 
     return () => window.clearTimeout(id);
-  }, [hide, handleHide]);
+  }, [hide, handleHide, speed]);
 
   return (
     <div
       className={`${styles.modal} ${fadeIn && !hide ? styles.visible : null}`}
+      style={{ transition: `opacity ${speed}ms ease` }}
     >
       <div className={styles.wrapper}>
-        <button className={styles.close} onClick={() => setHide(true)}>
-          X
-        </button>
+        {withCloseButton && (
+          <button className={styles.close} onClick={() => setHide(true)}>
+            X
+          </button>
+        )}
         {children}
       </div>
     </div>
@@ -36,10 +45,12 @@ export default function Modal({ handleHide, children }) {
 }
 
 Modal.propTypes = {
-  handleHide: PropTypes.func.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.object,
     PropTypes.array,
   ]),
+  handleHide: PropTypes.func,
+  withCloseButton: PropTypes.bool,
+  speed: PropTypes.number,
 };
