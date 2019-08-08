6fbe325de9ee9865106f2aad58c5c5d5f8532871
pasdo501 <dominikpaschke@hotmail.com>
2019-08-08 23:39:40 +1200

    Test component with Grid display for nicer board

---
+++ src/components/GridTest.js
@@ -0,0 +1,151 @@
+import React from "react";
+
+export default function GridTest() {
+    return (
+        <div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+            <div className="row">
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+                <div className="col">x</div>
+            </div>
+        </div>
+    )
+}
