c6bdd26af7aefe5bb5762a7a06be254f66f15546
pasdo501 <dominikpaschke@hotmail.com>
2020-02-15 19:44:56 +1300

    Update Server location

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -5,7 +5,7 @@ import FlashState from "../util/FlashState";
 
 const hostAddr =
   process.env.NODE_ENV === "production"
-    ? "http://battleship-server-battleship-server.apps.ca-central-1.starter.openshift-online.com/"
+    ? "https://node-battleship-server.herokuapp.com"
     : "localhost:8080";
 
 export default function useSocket() {
